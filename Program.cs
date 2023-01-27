c4073faa61ea63bef7c77eb02e72f672230d9505
User <User@K108>
2023-01-27 11:26:54 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadacha
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Program hello = new Program();
+            hello.WriteHello();
+          
+        }
+        public void WriteHello()
+        {
+            Console.WriteLine("Hello, C#!");
+        }
+    }
+}
